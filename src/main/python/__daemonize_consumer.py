95158747b6527b2affbf036e43200f958fc0c0aa
Adhir Potdar <adhir.potdar@bito.ai>
2024-08-22 13:55:25 +0530

    deleged.

--- src/main/python/__daemonize_consumer.py
+++
@@ -1,8 +0,0 @@
-import daemon
-from __consumer import ConsumeData
-
-if __name__ == "__main__":
-    with daemon.DaemonContext():
-        consume_data = ConsumeData()
-        consume_data.consumeUserData()
-
--- src/main/python/__insert_update_misc.py
+++
@@ -1,7 +0,0 @@
-from __insert_message_db import *
-
-
-if __name__ == "__main__":
-    insert_data = InsertData()
-    insert_data.consumeUserData()
-
