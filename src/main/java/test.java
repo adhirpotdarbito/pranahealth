dfd397968ec04d1c99b687289a36f40e8be49584
Adhir Potdar <adhir.potdar@bito.ai>
2024-08-22 15:42:01 +0530

    Added a test data funciton.

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,19 @@ public class test {
     public test() {
     }
 
+    public testData() {
+        String a = null; 
+        a.split(","); 
+
+        FileInputStream f = null; 
+        try { 
+            f.read(); 
+        }
+        catch (IOException e) 
+        { 
+        }
+    }
+
     public static void main(String [] args) {
         System.out.println("Testing code...");
     }
